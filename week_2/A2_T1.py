e57c145ab5d0e6d278a52cb5595477eba0de35f2
Nguyen Cat Duc Khiem <duckhiem373@gmail.com>
2026-09-19 12:47:12 +0300

    a2 t1 done

---
+++ week_2/A2_T1.py
@@ -0,0 +1,8 @@
+name = input("What is your name: ")
+num1 = float(input("Enter a floating point number: "))
+num2 = float(input("Enter second floating point number: "))
+
+product = round((num1 * num2), 2)
+
+print(f"{name} you gave numbers {num1} and {num2}")
+print(f"Multiplying first and second number will give result in product {product}")
