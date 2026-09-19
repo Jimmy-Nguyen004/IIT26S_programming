b48fe63287580d6d4bd2422af1dd9bf288a51025
Nguyen Cat Duc Khiem <duckhiem373@gmail.com>
2026-09-19 13:05:41 +0300

    a2 t2 done

---
+++ week_2/A2_T2.py
@@ -0,0 +1,9 @@
+print("Program starting.")
+
+car_brand = input("Insert car brand: ")
+car_model = input("Insert car model: ")
+
+print('Car brand is "', car_brand, '"', sep="", end=" and ")
+print("the model is '", car_model, "'.", sep="")
+
+print("Program ending.")
