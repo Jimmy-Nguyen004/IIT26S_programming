ee2cf30940d64a44f064ffd53fe8cc8a18e47ef3
Nguyen Cat Duc Khiem <duckhiem373@gmail.com>
2026-09-19 13:10:42 +0300

    a2 t3 done

---
+++ week_2/A2_T3.py
@@ -0,0 +1,11 @@
+
+
+firstword = input("Insert first word: ")
+secondword = input("Insert second word: ")
+
+print("1st word is", len(firstword), "characters long.")
+print("2nd word is", len(secondword), "characters long.")
+
+compound = firstword + secondword
+print("Words together makes one closed compound '", compound, "'.", sep="")
+
